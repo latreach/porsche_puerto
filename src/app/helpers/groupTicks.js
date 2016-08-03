2849052cf1f69b6ce79d1b43f00b783ad0ede9b7
Jhosse Paul Márquez Ruíz <mcromanceva@hotmail.com>
2016-08-03 12:27:57 -0500

    Fix: ';' removido y corrección de dependencias

--- src/app/helpers/groupTicks.js
+++ src/app/helpers/groupTicks.js
@@ -1,15 +1,17 @@
 /**
  * Returns an array of tick angles and labels, given a group
  * @param  {Object} d - A D3 array, expected endAngle, startAngle and value
-* properties
+ *      properties
  * @returns {Array}
  */
-export default function(d) {
+import {range} from 'd3';
+export default function (d) {
   const k = (d.endAngle - d.startAngle) / d.value;
-  return d3.range(0, d.value, 1).map( (v, i) =>
+
+  return range(0, d.value, 1).map((v, i) =>
     ({
       angle: v * k + d.startAngle,
       label: i % 5 ? null : v + '%'
-    });
-  });
+    })
+  );
 }
