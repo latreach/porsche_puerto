4a2eedc718956b44b33eda66fd765b91756a6dd7
FerDoranNie <fertimjim@gmail.com>
2016-08-12 00:50:59 -0500

    cambios a la gráfica de donas que no se ve :anguished:

--- src/app/concesionarias/donutpie.js
+++ src/app/concesionarias/donutpie.js
@@ -9,8 +9,7 @@ export const arc = d3.arc()
   .outerRadius(radius - 70);
 export const pie = d3.pie()
   .value(function(d){return d.dimension;});
-export const svg = d3.select("#concesionarias-animacion").append('div')
-  .attr('class', 'svg-container')
+export const svg = d3.select("#concesionarias-animacion")
   .append('svg')
   .attr("width", width)
   .attr("height", height)
@@ -21,11 +20,11 @@ export function type(d){
   d.dimension = +d.dimension;
   return d;
 }
-
+console.log("ora ya")
 d3.csv('./assets/data/pie_data.csv', type, function(error, data){
   if (error){
     throw error;
-  }
+  };
   const g = svg.selectAll(".arc")
     .data(pie(data))
     .enter().append("g")
