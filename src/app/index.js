c66fc1416693d752389eb449948331a9d176fff7
FerDoranNie <fertimjim@gmail.com>
2016-08-12 10:56:34 -0500

    Modificando la leyenda aún no queda

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+import './intro';
+import './iot';
+import './tecnologia_singularidad';
+import './concesionarias';
+//import './energia';
+import './tecnologia_singularidad';
+
+
+
+
--- src/app/tecnologia_singularidad/linear.js
+++ src/app/tecnologia_singularidad/linear.js
@@ -1,14 +1,14 @@
 export const margin = {top: 20, right: 80, bottom: 30, left: 50};
 export const width = 1500 - margin.left - margin.right;
-export const height = 600 - margin.top - margin.bottom;
+export const height = 500 - margin.top - margin.bottom;
 export const svg = d3.select('#tecnologia-animation')
   .append('div')
   .attr('class', 'svg-container')
   .append('svg')
   .attr(
-    'viewBox', 
-    [0,0,window.innerWidth, window.innerHeight -55].join(' ')
-    )
+  'viewBox', 
+  [0,0,window.innerWidth, window.innerHeight -55].join(' ')
+  )
   .attr('width', width + margin.right + margin.left)
   .attr('height', height + margin.top + margin.bottom)
   .attr('preserveAspectRatio', 'xMidYMid meet')
@@ -43,62 +43,9 @@ const formatPower = function(d) {
 };
 
 
-export const d3.legend = function(g) {
-  g.each(function() {
-    var g= d3.select(this),
-        items = {},
-        svg = d3.select(g.property("nearestViewportElement")),
-        legendPadding = g.attr("data-style-padding") || 5,
-        lb = g.selectAll(".legend-box").data([true]),
-        li = g.selectAll(".legend-items").data([true])
-
-    lb.enter().append("rect").classed("legend-box",true)
-    li.enter().append("g").classed("legend-items",true)
-
-    svg.selectAll("[data-legend]").each(function() {
-        var self = d3.select(this)
-        items[self.attr("data-legend")] = {
-          pos : self.attr("data-legend-pos") || this.getBBox().y,
-          color : self.attr("data-legend-color") != undefined ? self.attr("data-legend-color") : self.style("fill") != 'none' ? self.style("fill") : self.style("stroke") 
-        }
-      })
-
-    items = d3.entries(items).sort(function(a,b) { return a.value.pos-b.value.pos})
-
-    
-    li.selectAll("text")
-        .data(items,function(d) { return d.key})
-        .call(function(d) { d.enter().append("text")})
-        .call(function(d) { d.exit().remove()})
-        .attr("y",function(d,i) { return i+"em"})
-        .attr("x","1em")
-        .text(function(d) { ;return d.key})
-    
-    li.selectAll("circle")
-        .data(items,function(d) { return d.key})
-        .call(function(d) { d.enter().append("circle")})
-        .call(function(d) { d.exit().remove()})
-        .attr("cy",function(d,i) { return i-0.25+"em"})
-        .attr("cx",0)
-        .attr("r","0.4em")
-        .style("fill",function(d) { console.log(d.value.color);return d.value.color})  
-    
-    // Reposition and resize the box
-    var lbbox = li[0][0].getBBox()  
-    lb.attr("x",(lbbox.x-legendPadding))
-        .attr("y",(lbbox.y-legendPadding))
-        .attr("height",(lbbox.height+2*legendPadding))
-        .attr("width",(lbbox.width+2*legendPadding))
-  })
-  return g
-}
 
 
-
-
-
-
-d3.csv('./assets/data/linear_1.csv', type, function (error, data) {
+d3.csv('./assets/data/linear_2.csv', type, function (error, data) {
   if (error) {
     throw error;
   }
@@ -154,13 +101,19 @@ d3.csv('./assets/data/linear_1.csv', type, function (error, data) {
       .text('');
 
   //modifcando el texto de los ejes
-  g.selectAll('.axis text')
+  g.selectAll('.axis--y text')
     .attr('transform', 'rotate(0)')
     .style('font', '20px sans-serif');
+  
   const linea = g.selectAll('.linea')
     .data(linear).enter()
     .append('g')
-      .attr('class', 'linea');
+    .attr('class', 'linea');
+
+  g.selectAll('.axis--x text')
+    .attr('transform', 'rotate(0)')
+    .style('font', '18px sans-serif');
+
 
   linea.append('path')
     .attr('class', 'line')
@@ -180,41 +133,38 @@ d3.csv('./assets/data/linear_1.csv', type, function (error, data) {
         });
     });
 
-  linea.append('text')
-    .datum(function (d) {
-      return {id: d.id, value: d.values[d.values.length - 1]};
-    })
-    .attr('transform', function (d) {
-      return 'translate(' + x(d.value.tiempo) + ',' + y(d.value.linear) + ')';
-     })
-    .attr('x', 3)
-    .attr('dy', '0.75em')
-    .style('font', '25px ubuntu')
-    .style('font-style', 'italic')
-   .each(function (d, i) {
-      d3.select(this)
-       .transition().duration(7000).delay(3500 * i)
-    .style('fill', function (f) {
-       return z(f.id);
-     });
-   })
-    .text(function(d){return d.id;});
+  //linea.append('text')
+  // .datum(function(d){
+  //   return {id: d.id, value: d.values[d.values.length -1]};
+  // })
+  // .attr("transform", function(d){ "translate(" + x(d.value.tiempo) + "," + y(d.value.linear)  + ")"} )
+  // .attr('x', 3)
+  // .attr('dy', '0.75em')
+  // .style("font", "12px ubuntu")
+  // .text(function(d){return d.id});
 
-  legend = svg,append("g")
-    .attr("class", "legend")
-    .attr("transform", "translate(50,30)")
-    .style("font-size", "12px")
-    .call(d3.legend)
-
-  setTimeOut(function(){
-    legend
-      .style("font-size", "12px")
-      .attr("data-style-padding", 10)
-      .call(d3.legend)
-  },1000
-  )
-
-
+  //  linea.append('text')
+  // .datum(function (d) {
+  //   return {id: d.id, value: d.values[d.values.length - 1]};
+  // })
+  //.attr('transform', function (d) {
+  //  return 'translate(' + x(d.value.tiempo) + ',' + y(d.value.linear) + ')';
+  //}
+  // )     
+  // .attr('x', 3)
+  // .attr('dy', '0.75em')
+  // .style('font', '25px ubuntu')
+  // .style('font-style', 'italic')
+  // .each(function (d, i) {
+  //   d3.select(this)
+  //   .transition().duration(7000).delay(3500 * i)
+  // .style('fill', function (f) {
+  //   return z(f.id);
+  // });
+  // })
+  // .text(function(d){return d.id;});
 
 
 });
+
+
